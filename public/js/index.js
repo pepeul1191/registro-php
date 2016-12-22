73b57a7cc81db822c248ca0f33b83129140c8f56
Pepe Valdivia <jvaldivia@softweb.pe>
2016-12-22 13:25:25 -0500

    Comenzando el formulario

--- public/js/index.js
+++ public/js/index.js
@@ -1,56 +1,38 @@
 var BASE_URL = "http://localhost/investigadores/";
 
-
-$(document).on("click", "#btnEjecutar",function() {
-	var incluir_acciones = $("#chkAcciones").is(':checked');
-
-    var query = $("#txtQuery").val();
-    $.ajax({
-        type: "POST",
-        url: BASE_URL + "query",
-        data: "data=" + JSON.stringify(query),
-        async: false,
-        success:function(data){
-           var rset = JSON.parse(data);
-			var keys = [];
-			var rptaString = "<table class='table table-striped'><thead>";
-			var acciones = [];
-
-			for(var k in rset[0]){ 
-			 	keys.push(k);
-			 	var temp = "<th>" + k + "</th>";
-			 	rptaString = rptaString + temp;
-			}
-
-			if(incluir_acciones == true){	
-				var temp = "<th>Acciones</th>";
-				rptaString = rptaString + temp;
-			}
-
-			rptaString = rptaString + "</thead>";
-
-			for(var i = 0; i < rset.length ; i++){
-				var row = rset[i];
-				rptaString = rptaString + "<tr>";
-
-				for(var j = 0; j < keys.length; j++){
-					var temp = "<td>" + row[keys[j]] + "</td>";
-					rptaString = rptaString + temp;
-				}
-				
-				if(incluir_acciones == true){	
-					if(typeof row["id"] !== 'undefined'){
-						var id = row["id"];
-						var temp = "<td>" + "<a class='accion' href='" + BASE_URL + "ver/" + id + "'><i class='fa fa-search' aria-hidden='true'></i></a>" + "<a class='accion' href='" + BASE_URL + "editar/" + id + "'><i class='fa fa-pencil' aria-hidden='true'></i></a>" + "</td>";
-						rptaString = rptaString + temp;
-					}
-				}
-
-				rptaString = rptaString + "</tr>";
-			}
-
-			$("#tablaResultSet").empty();
-			$("#tablaResultSet").append(rptaString);
-        }
-    });
+$(document).on("click", "#chkAcepto",function(event){
+	var checkado =$('#chkAcepto:checkbox:checked').length > 0;
+	if (checkado){
+		$("#btnEnviar").prop("disabled", false); 
+	}else{
+		$("#btnEnviar").prop("disabled", true);
+	}
 });
+
+$(document).on("click", "#btnEnviar",function() {
+	var valido = validarCorreo($("#txtCorreo").val());
+	if (valido){
+		$("#txtCorreo").removeClass("input-text-error");
+		$("#txtCorreo").attr("placeholder", "Correo Electrónico");
+		/*
+		$.ajax({
+	        type: "POST",
+	        url: BASE_URL + "query",
+	        data: "data=" + JSON.stringify(query),
+	        async: false,
+	        success:function(data){
+
+	        }
+	    });
+		*/
+	}else{
+		$("#txtCorreo").addClass("input-text-error");
+		$("#txtCorreo").val("");
+		$("#txtCorreo").attr("placeholder", "Ingrese un dirección de correo válida");
+	}
+});
+
+function validarCorreo(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
